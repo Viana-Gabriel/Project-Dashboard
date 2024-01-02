5272e902e2501a251f56c8d527c07ec395b598f4
Gabriel Viana Santos <108957443+Viana-Gabriel@users.noreply.github.com>
2024-01-02 14:53:02 -0300

    Adicionado rotas
    rotas foram adicionadas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,10 @@
-export{};
+import { Route, Routes } from "react-router-dom";
+
+export const AppRoutes = ()=>{
+  return(
+    <Routes>
+      <Route path="/pagina-inicial" element={<p>Pagina Inicial</p>}></Route>
+      <Route path="*" element={<p>Pagina nao encontrada</p>}></Route>
+    </Routes>
+  )
+};
