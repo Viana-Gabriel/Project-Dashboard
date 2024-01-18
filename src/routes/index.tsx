460bc9c7c7cb554c72a03111e082ddf0ba192b16
Gabriel Viana Santos <108957443+Viana-Gabriel@users.noreply.github.com>
2024-01-18 18:32:17 -0300

    Add Theme dark
    Adicao do tema escura ao projeto

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
+import { useAppThemeContext } from "../shared/contexts";
 
 export const AppRoutes = ()=>{
+  const {toggleTheme} = useAppThemeContext()
   return(
     <Routes>
-      <Route path="/pagina-inicial" element={<Button variant="contained" color="primary">Teste</Button>}></Route>
+      <Route path="/pagina-inicial" element={<Button variant="contained" color="primary" onClick={toggleTheme}>Teste</Button>}></Route>
       <Route path="*" element={<p>Pagina nao encontrada</p>}></Route>
     </Routes>
   )
